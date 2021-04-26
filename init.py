0632a5639683d63f2bd5b09e0877849207e0ddcd
Exanut <nihilch@protonmail.com>
2021-04-26 16:01:07 -0400

    message input formatting

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ import discord
 import os
 
 from secret import TOKEN
+cmd_prefix = '!'
 
 client = discord.Client()
 
@@ -16,5 +17,18 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith(cmd_prefix + 'c'):
+        valid_input = True
+        try:
+            query = message.content[3:]
+            query = query.strip().lower()
+            assert (query and query != '')
+        except:
+            valid_input = False
+        if not valid_input:
+            return
+        
+        
 
 client.run(TOKEN)
