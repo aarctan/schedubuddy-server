17108c97f137a2fdf2cdeb8e33671b10b94cde95
Exanut <nihilch@protonmail.com>
2021-04-29 03:34:11 -0400

    faster interval constraint check

--- util/permutation.py
+++ util/permutation.py
@@ -1,22 +1,20 @@
 from query import get_course_classes
 import itertools
 
-def days_there_are_classes(classes):
-    days = set([])
-    for course_class in classes:
-        for day in course_class[6]:
-            days.add(day)
-    return days
-
-def conflict_on_day(classes):
+day_lookup = {'M':0, 'T':1, 'W':2, 'R':3, 'F':4, 'S':5, 'U':6}
+def valid_schedule(sched):
     ranges = []
-    for course_class in classes:
-        ranges.append((course_class[4], course_class[5]))
+    for course in sched:
+        for course_class in course:
+            start_t, end_t = course_class[4], course_class[5]
+            for day in course_class[6]:
+                day_mult = day_lookup[day]
+                ranges.append((start_t + 2400*day_mult, end_t + 2400*day_mult))
     ranges.sort(key=lambda t: t[0])
     for i in range(len(ranges)-1):
-        if ranges[i+1][0] < ranges[i][0]:
-            return True
-    return False
+        if ranges[i+1][0] < ranges[i][1]:
+            return False
+    return True
 
 def permute_classes(str_courses):
     course_prods = []
@@ -28,28 +26,18 @@ def permute_classes(str_courses):
     sched_prod = list(itertools.product(*course_prods))
     valid_schedules = []
     for sched in sched_prod:
-        classes = []
-        for course in sched:
-            for course_class in course:
-                classes.append(course_class)
-        class_days = days_there_are_classes(classes)
-        valid_sched = True
-        for day in class_days:
-            classes_on_day = []
-            for course_class in classes:
-                if day in course_class[6]:
-                    classes_on_day.append(course_class)
-            if conflict_on_day(classes_on_day):
-                valid_sched = False
-                break
-        if valid_sched:
+        if valid_schedule(sched):
             valid_schedules.append(sched)
     return valid_schedules
 
+#schedules = permute_classes(["CHEM 101", "STAT 151", "PSYCO 258", "PSYCO 223", "PSYCO 275"])
+schedules = permute_classes(["CMPUT 397", "CMPUT 355", "PSYCO 258", "PSYCO 223", "PSYCO 275"])
 
-schedules = permute_classes(["CHEM 102", "CHEM 261"])
 for schedule in schedules:
-    print(schedule)
+    for course in schedule:
+        print(course)
     print()
+print(len(schedules))
+
 
 
