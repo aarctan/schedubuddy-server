ed48095967dc5cd0f5e4ce0259f294a4da651365
Exanut <nihilch@protonmail.com>
2021-04-29 05:19:31 -0400

    Some work on drawing basic classtimes

--- util/draw_sched.py
+++ util/draw_sched.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageDraw
 left_margin_offset = 75
 top_margin_offset = 45
 box_width = 103
-vertical_length_50 = 95
+vertical_length_50 = 49
 vertical_length_80 = 75
 
 RED = (255,153,153)
@@ -26,11 +26,16 @@ def draw_schedule(sched):
             for course_class in course:
                 days = course_class[6]
                 for day in days:                    
+                    start_t, end_t = course_class[4], course_class[5]
                     r_x0 = left_margin_offset + day_lookup[day] * box_width + day_lookup[day]*2
-                    r_y0 = top_margin_offset + 0
+                    r_y0 = 0
+                    if start_t % 60 == 0:
+                        hours = (start_t // 60 - 8)
+                        r_y0 = top_margin_offset + hours*vertical_length_50 + hours*2
+                        if hours > 0: # Manual y offset adjust 
+                            r_y0 += 1
                     r_x1 = r_x0 + box_width
-                    class_len = length_lookup[course_class[5] - course_class[4]]
-                    r_y1 = r_y0 + class_len
+                    r_y1 = r_y0 + length_lookup[end_t - start_t]
                     draw.rectangle([(r_x0, r_y0), (r_x1, r_y1)], fill = color)
             colors += 1
 
--- util/driver.py
+++ util/driver.py
@@ -1,7 +1,7 @@
 from permutation import permute_classes
 from draw_sched import draw_schedule
 
-schedules = permute_classes(["CMPUT 355"])
+schedules = permute_classes(["CMPUT 355", "CMPUT 397", "PSYCO 223"])
 
 first_sched = schedules[0]
 print(first_sched)
