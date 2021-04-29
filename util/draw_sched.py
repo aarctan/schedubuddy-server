37ade7271f915b8454c117262214018bc59d64cc
Exanut <nihilch@protonmail.com>
2021-04-29 19:53:07 -0400

    fix boilerplate to have normal pixel length; draw 50/80/170 len classes

--- util/draw_sched.py
+++ util/draw_sched.py
@@ -3,9 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 left_margin_offset = 75
 top_margin_offset = 45
 box_width = 103
-vertical_length_30 = 25
-vertical_length_50 = 49
-vertical_length_80 = 75
+vertical_length_50 = 50
 
 RED = (255,153,153)
 YELLOW = (254,255,153)
@@ -16,8 +14,24 @@ PINK = (255,153,204)
 
 color_scheme = (RED, YELLOW, GREEN, BLUE, PURPLE, PINK)
 day_lookup = {'M':0, 'T':1, 'W':2, 'R':3, 'F':4, 'S':5, 'U':6}
-length_lookup = {0:0, 30:vertical_length_30, 50:vertical_length_50, 80:vertical_length_80}
+length_lookup = {
+    0:      0,
+    30:     25,
+    50:     vertical_length_50,
+    80:     76,
+    170:    154,
+    180:    101
+}
 font = ImageFont.truetype("../fonts/tahoma.ttf", 11)
+
+def get_draw_text(course_class):
+    course_name = course_class[8]
+    class_component = course_class[0]
+    class_section = course_class[1]
+    class_id = course_class[9]
+    text = course_name + '\n' + class_component + ' ' + class_section +\
+        ' (' + class_id + ')'
+    return text
 
 def draw_schedule(sched):
     with Image.open("../boilerplate.png") as image:
@@ -37,21 +51,20 @@ def draw_schedule(sched):
                     r_y0 = 0
                     hours = (start_t // 60 - 8)
                     minutes = (start_t % 60)
-                    r_y0 = top_margin_offset + hours*vertical_length_50 + hours*2
+                    bars_past = 0
+                    if day in 'MWF':
+                        bars_past = max(0, hours*2-1)
+                    elif day in 'TR':
+                        tr_offset = (start_t-480)//90
+                        bars_past = max(0, tr_offset*2+tr_offset-1)
+                    r_y0 = top_margin_offset + hours*vertical_length_50 + bars_past
                     r_y0 += length_lookup[minutes]
                     if hours > 0: # Manual y offset adjust 
                         r_y0 += 1
 
                     r_y1 = r_y0 + length_lookup[end_t - start_t]
                     draw.rectangle([(r_x0, r_y0), (r_x1, r_y1)], fill = color)
-
-                    course_name = course_class[8]
-                    class_component = course_class[0]
-                    class_section = course_class[1]
-                    class_id = course_class[9]
-                    text = course_name + '\n' + class_component + ' ' + class_section +\
-                        ' (' + class_id + ')'
-                    draw.text((r_x0+2, r_y0+2), text, (0,0,0), font=font)
+                    draw.text((r_x0+2, r_y0+2), get_draw_text(course_class), (0,0,0), font=font)
             colors += 1
 
         image.save("schedule.png")
--- util/driver.py
+++ util/driver.py
@@ -1,9 +1,18 @@
 from permutation import permute_classes
 from draw_sched import draw_schedule
 
-schedules = permute_classes(["CMPUT 355", "CMPUT 397", "PSYCO 223"])
+basic_schedules1 = permute_classes(["CMPUT 355", "CMPUT 397", "PSYCO 223", "PSYCO 258", "PSYCO 275"])
+b_s1 = basic_schedules1[0]
 
-first_sched = schedules[0]
-print(first_sched)
+basic_schedules2 = permute_classes(["CHEM 102"])
+b_s2 = basic_schedules2[3]
 
-draw_schedule(first_sched)
+mwf_offset = 0
+tf_offset = 0
+manual_sched1 = (( ['LEC', 'A1', 'REMOTE', 'Melis Gedik', 480+60*mwf_offset, 530+60*mwf_offset, 'MWF', None, 'CHEM 102', '75282'],
+['LAB', 'W6', 'REMOTE', 'Yoram Apelblat', 480+90*tf_offset, 650+90*tf_offset, 'T', None, 'CHEM 102', '86592']),)
+
+
+draw_schedule(b_s1)
+
+
