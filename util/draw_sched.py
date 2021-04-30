9646af94e2b06a77e172cbc81d8d87cfb2e76a98
Exanut <nihilch@protonmail.com>
2021-04-30 00:10:59 -0400

    crop utility; replace instructor with location

--- util/draw_sched.py
+++ util/draw_sched.py
@@ -1,9 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-
-left_margin_offset = 75
-top_margin_offset = 45
-box_width = 103
-vertical_length_50 = 50
+from math import ceil
 
 RED = (255,153,153)
 YELLOW = (254,255,153)
@@ -11,6 +7,12 @@ GREEN = (153,255,152)
 BLUE = (153,204,254)
 PURPLE = (204,153,255)
 PINK = (255,153,204)
+HOUR_PADDING = 2
+
+left_margin_offset = 75
+top_margin_offset = 45
+box_width = 103
+vertical_length_50 = 50
 
 color_scheme = (RED, YELLOW, GREEN, BLUE, PURPLE, PINK)
 day_lookup = {'M':0, 'T':1, 'W':2, 'R':3, 'F':4, 'S':5, 'U':6}
@@ -19,8 +21,9 @@ length_lookup = {
     30:     25,
     50:     vertical_length_50,
     80:     76,
+    110:    101,
     170:    154,
-    180:    101
+    180:    101,
 }
 font = ImageFont.truetype("../fonts/tahoma.ttf", 11)
 
@@ -42,18 +45,21 @@ def get_draw_text(course_class):
                 instructor_text = instructor_text[:-1]
             instructor_text += '...'
         instructor_text_size = font.getsize(instructor_text)
+    location = course_class[2]
     text = course_name + '\n' + class_component + ' ' + class_section +\
-        ' (' + class_id + ')\n' + instructor_text
-    return text
+        ' (' + class_id + ')\n' + location
+    return text.upper()
 
 def draw_schedule(sched):
     with Image.open("../boilerplate.png") as image:
         draw = ImageDraw.Draw(image)
         colors = 0
+        max_end_t = -1
         for course in sched:
             color = color_scheme[colors%6]
             for course_class in course:
                 start_t, end_t = course_class[4], course_class[5]
+                max_end_t = max(max_end_t, end_t)
                 if end_t == -1: # Asynchronous classes
                     continue
                 days = course_class[6]
@@ -79,5 +85,10 @@ def draw_schedule(sched):
                     draw.rectangle([(r_x0, r_y0), (r_x1, r_y1)], fill = color)
                     draw.text((r_x0+2, r_y0+2), get_draw_text(course_class), (0,0,0), font=font)
             colors += 1
-
-        image.save("schedule.png")
+        w,h = image.size
+        hours_cropped = 17-(ceil(max_end_t/60)-8 + HOUR_PADDING)
+        crop_line = h - (hours_cropped*51 + hours_cropped)
+        print(hours_cropped)
+        draw.line((0,crop_line-1, w, crop_line-1), fill=(0, 0, 0), width=1)
+        image.crop((0, 0, w, crop_line)).save("schedule.png")
+#        image.save("schedule.png")
