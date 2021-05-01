4fd9fb95cd25cdb8edd4f573d658c580ee52e4e0
Exanut <nihilch@protonmail.com>
2021-05-01 05:32:39 -0400

    crop weekends if no class on weekends

--- draw_sched.py
+++ draw_sched.py
@@ -24,6 +24,8 @@ length_lookup = {
     110:    101,
     170:    309,
     180:    309,
+    240:    414,
+    420:    725,
 }
 font = ImageFont.truetype("fonts/tahoma.ttf", 19)
 
@@ -56,6 +58,7 @@ def draw_schedule(sched):
         colors = 0
         min_y = 2147483647
         max_y = -2147483648
+        class_on_weekend = False
         for course in sched:
             color = color_scheme[colors%6]
             for course_class in course:
@@ -65,7 +68,9 @@ def draw_schedule(sched):
                 if end_t == -1: # Asynchronous classes
                     continue
                 days = course_class[6]
-                for day in days:               
+                for day in days:
+                    if day == 'S' or day == 'U':
+                        class_on_weekend = True
                     r_x0 = left_margin_offset + day_lookup[day] * box_width + day_lookup[day]*2
                     r_x1 = r_x0 + box_width-1
 
@@ -80,13 +85,14 @@ def draw_schedule(sched):
                         r_y0 += length_lookup[minutes]
                         incr_90 = start_t // 90
                         r_y0 += incr_90*length_lookup[80] + incr_90*2
-
+                
                     r_y1 = r_y0 + length_lookup[end_t - start_t]
                     draw.rectangle([(r_x0-2, r_y0-2), (r_x1+2, r_y1+2)], fill=(0,0,0))
                     draw.rectangle([(r_x0, r_y0), (r_x1, r_y1)], fill = color)
-                    draw.text((r_x0+3, r_y0+2), get_draw_text(course_class), (0,0,0), font=font)
-                    
+                    draw.text((r_x0+4, r_y0+2), get_draw_text(course_class), (0,0,0), font=font)
             colors += 1
+
+        # get the y region
         boilerplate_width, boilerplate_height = image.size
         top_hours = min(8, floor(min_y/60))
         y_region_top = top_margin_offset + top_hours * vertical_length_50 + top_hours*3
@@ -94,8 +100,18 @@ def draw_schedule(sched):
         y_region_bottom = top_margin_offset + bottom_hours * vertical_length_50 + bottom_hours*3
         y_region = image.crop((0, y_region_top, boilerplate_width, y_region_bottom))
         y_region_length = y_region_bottom-y_region_top
-
+        # paste the y region
         image.paste(y_region, (0, top_margin_offset, boilerplate_width, top_margin_offset+y_region_length))
         y_crop_line = top_margin_offset + y_region_length
         draw.line((0,y_crop_line-2, boilerplate_width, y_crop_line-2), fill=(0, 0, 0), width=2)
-        image.crop((0, 0, boilerplate_width, y_crop_line)).save("schedule.png")
+        image = image.crop((0, 0, boilerplate_width, y_crop_line))
+        # crop weekend if no classes
+        if not class_on_weekend:
+            x_region_left = left_margin_offset + box_width + 2
+            x_region_right = boilerplate_width - box_width - 2
+            x_region_length = x_region_right - x_region_left
+            x_region = image.crop((x_region_left, 0, x_region_right, y_crop_line))
+            image.paste(x_region, (left_margin_offset, 0, left_margin_offset+x_region_length, y_crop_line))
+            image = image.crop((0, 0, left_margin_offset + x_region_length, y_crop_line))
+        
+        image.save("schedule.png")
