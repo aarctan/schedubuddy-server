305b097ec730d982ab1040b4cf5928702ede8e11
Exanut <nihilch@protonmail.com>
2021-05-11 07:35:21 -0400

    wrapping pages

--- schedule_session.py
+++ schedule_session.py
@@ -1,4 +1,5 @@
 import sched_gen
+import draw_sched
 
 from contextlib import suppress
 import asyncio
@@ -36,31 +37,31 @@ class ScheduleSession:
         courses_queried = [x+' '+y for x,y in zip(args[0::2], args[1::2])]
         return sched_gen.generate_schedules(courses_queried)
 
-    def embed_page(self, page_number: int = 0) -> Embed:
+    async def update_page(self, page_number: int = 0) -> None:
+        self._current_page = page_number
         msg_desc = "Listing **{}** of **{}** generated schedules for the\
             specified course selection:"\
             .format(str(page_number+1), str(len(self.schedules)))
         embed = Embed(description=msg_desc, color=0xb3edbd)
-        return embed
-
-    async def update_page(self, page_number: int = 0) -> None:
-        self._current_page = page_number
-        embed_page = self.embed_page(page_number)
-        if not self.message:
-            self.message = await self.destination.send(embed=embed_page)
-        else:
-            await self.message.edit(embed=embed_page)
+        embed.set_author(name=self.author.display_name,
+                        icon_url=self.author.avatar_url)
+        draw_sched.draw_schedule(self.schedules[page_number])
+        file = discord.File("schedule.png", filename="image.png")
+        embed.set_image(url="attachment://image.png")
+        self.new_message = await self.destination.send(file=file, embed=embed)
+        if self.message:
+            await self.message.delete()
+        self.message = self.new_message
+        self._bot.loop.create_task(self.message.add_reaction(LEFT_EMOJI))
+        self._bot.loop.create_task(self.message.add_reaction(RIGHT_EMOJI))
 
     async def build_pages(self) -> None:
-        page = {}
         self._pages = self.schedules
 
     async def prepare(self) -> None:
         await self.build_pages()
-        self._bot.add_listener(self.on_reaction_add)
         await self.update_page()
-        self._bot.loop.create_task(self.message.add_reaction(LEFT_EMOJI))
-        self._bot.loop.create_task(self.message.add_reaction(RIGHT_EMOJI))
+        self._bot.add_listener(self.on_reaction_add)
 
     @classmethod
     async def start(cls, ctx: Context, *command, **options) -> "ScheduleSession":
@@ -85,10 +86,14 @@ class ScheduleSession:
     async def do_back(self) -> None:
         if self._current_page != 0:
             await self.update_page(self._current_page-1)
+        else:
+            await self.update_page(len(self._pages)-1)
 
     async def do_next(self) -> None:
         if self._current_page != (len(self._pages)-1):
             await self.update_page(self._current_page+1)
+        else:
+            await self.update_page(0)
 
     async def on_reaction_add(self, reaction: Reaction, user: User) -> None:
         emoji = str(reaction.emoji)
