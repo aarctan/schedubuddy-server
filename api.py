1d15bdeae10a3ea56a6ed6ae2b2a19557b088c5c
Exanut <nihilch@protonmail.com>
2021-05-15 05:18:03 -0400

    api querying

--- api.py
+++ api.py
@@ -6,38 +6,16 @@ import sched_gen
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    (s, a) = sched_gen.generate_schedules(["CMPUT 174", "MATH 117", "MATH 127", "STAT 151", "WRS 101"])
-    json_schedules = [sched._schedule for sched in s[:3]]
-    return jsonify(json_schedules)
-    #return jsonify(books)
+# /api/v1/gen-schedules?query={"courses":["CMPUT 174", "MATH 117", "MATH 127", "STAT 151", "WRS 101"],"term":"Fall_21"}
+@app.route('/api/v1/gen-schedules', methods=['GET'])
+def api_id():
+    if 'query' in request.args:
+        query = eval(request.args['query'])
+        courses_queried = query["courses"]
+        (s, a) = sched_gen.generate_schedules(courses_queried)
+        json_schedules = [sched._schedule for sched in s]
+        return jsonify(json_schedules)
+    else:
+        return "Error: No query provided to gen-schedules."
 
 app.run()
