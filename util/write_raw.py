7f56737798f544e24ab0b76fb8f6480461e4c08e
Mattwmaster58 <mattwmaster58@gmail.com>
2023-07-24 23:10:28 -0400

    fix: black formatter

--- util/write_raw.py
+++ util/write_raw.py
@@ -3,7 +3,6 @@ import re, requests, os, json, time
 from bs4 import BeautifulSoup
 
 ROOT = "https://apps.ualberta.ca/catalogue"
-CATALOG_H2_CLASS = "[ fs-4 ][ flex-grow-1 ][ d-flex flex-column flex-md-row gap-2 ][ align-items-start align-items-md-center ]"
 TERM_DIV_CLASS = "card"
 TERM_H2_CLASS = "card-header"
 COMPONENT_DIV_CLASS = "col-12"
@@ -11,41 +10,52 @@ COMPONENT_H3_CLASS = "mt-2 d-none d-md-block"
 CLASS_DIV_CLASS = "col-lg-4 col-12 pb-3"
 CLASS_STRONG_CLASS = "mb-0 mt-4"
 
+
 # Returns a list of codes from a list on a webpage (i.e., faculty or subject codes)
-def get_link_codes_with_prefix(url, prefix):
+def get_link_codes_with_prefix(url: str, prefix: str):
     soup = BeautifulSoup(requests.get(url).content, "lxml")
-    soups = soup.findAll("a")
+    soups = soup.select("a[href]")
     codes = []
     for soup in soups:
-        if soup.has_attr("href"):
-            link = soup["href"]
-            if link[:len(prefix)] == prefix:
-                code = link[len(prefix)+1:].upper()
-                if not code:
-                    continue
-                codes.append(code)
+        link = soup["href"]
+        if link[:len(prefix)] == prefix:
+            code = link[len(prefix) + 1:].upper()
+            if not code:
+                continue
+            codes.append(code)
     return codes
+
 
 def get_faculties_from_catalogue():
     return get_link_codes_with_prefix(
         f"{ROOT}",
         "/catalogue/faculty")
 
+
 def get_subjects_from_faculty(faculty_code):
     return get_link_codes_with_prefix(
         f"{ROOT}/faculty/{faculty_code}",
         "/catalogue/course")
 
+
 # Returns a list of catalogs from a subject, e.g. "CMPUT" -> ['101', '174', ...]
 def get_catalogs_from_subject(subject):
     subject = "ZOOLE"
     courses_soup = BeautifulSoup(requests.get(f"{ROOT}/course/{subject}").content, "lxml")
-    course_titles = courses_soup.findAll("h2", {"class": CATALOG_H2_CLASS})
+    # .first class selects the first course of the card
+    # there are sometimes multiple items for card if the course is changing in the future
+    course_titles = courses_soup.select(".course.first a")
     catalogs = []
     for course_title in course_titles:
-        catalog = course_title.text.lstrip()[len(subject)+1:].split(' ')[0]
-        catalogs.append(catalog)
+        try:
+            # eg matches 331 in "CMPUT 331". asserts the subject prefixes the course number
+            pattern = re.escape(subject) + r" (\d{3,4}\w?)"
+            catalogs.append(re.search(pattern, course_title.text).group(1))
+        except AttributeError as e:
+            raise AttributeError(f"Failed to parse course title {course_title.text=}. "
+                                 f"Is the selector grabbing the wrong elements? {e=}")
     return catalogs
+
 
 def write_raw(subject, catalog, fp):
     class_objs = []
@@ -65,8 +75,8 @@ def write_raw(subject, catalog, fp):
             currChild += 1
         children = children[currChild:]
         for i in range(num_conts):
-            component_type = children[i*2].text[:3].upper()
-            component_table = children[i*2+1]
+            component_type = children[i * 2].text[:3].upper()
+            component_table = children[i * 2 + 1]
             headers = component_table.find_all('th')
             rows = {}
             for header in headers:
@@ -77,7 +87,7 @@ def write_raw(subject, catalog, fp):
                 row_soup = component_table.find_all("td", {"data-card-title": header_name})
                 row_count = len(row_soup)
                 for row in row_soup:
-                    col = row.find("div") # CONTENT
+                    col = row.find("div")  # CONTENT
                     if not col:
                         rows[header_name].append(None)
                         continue
@@ -86,11 +96,11 @@ def write_raw(subject, catalog, fp):
             rows = list(rows.values())
             for row_itr in range(row_count):
                 class_id, section, embeds = None, None, None
-                embeds = ["Capacity: 0"] # Do not use capacity, need column for compatibility with old db schema
+                embeds = ["Capacity: 0"]  # Do not use capacity, need column for compatibility with old db schema
                 candidate_row = [rows[i][row_itr] for i in range(3)]
                 for raw_candidate in candidate_row:
                     # col Section for class section and class id
-                    #print(raw_candidate)
+                    # print(raw_candidate)
                     if component_type in raw_candidate:
                         clean_raw_cand = list(filter(('').__ne__, raw_candidate.split(' ')))
                         has_syllabus = clean_raw_cand[-1].strip().lower() == "syllabus"
@@ -98,7 +108,7 @@ def write_raw(subject, catalog, fp):
                         class_id = clean_raw_cand[-4][1:-2] if has_syllabus else clean_raw_cand[2][1:-1]
                     # col Instructor(s); only deal with primary instructor
                     elif "Primary Instructor: " in raw_candidate:
-                        instructor_name = raw_candidate[len("Primary Instructor: ")+1:]
+                        instructor_name = raw_candidate[len("Primary Instructor: ") + 1:]
                         embeds.append(f"Primary Instructor: {instructor_name}")
                     # col for Dates + Times (embed)
                     elif re.search('\d+-\d+-\d+ \d+:\d+ - \d+:\d+', raw_candidate):
@@ -122,7 +132,10 @@ def write_raw(subject, catalog, fp):
                 class_objs.append(raw_obj)
     return class_objs
 
+
 debug = False
+
+
 def main():
     dirname = os.path.dirname(__file__)
     raw_file_path = os.path.join(dirname, "../local/raw.json")
@@ -136,27 +149,27 @@ def main():
         subjects = ['PL_SC']
     else:
         print(f"Reading faculties from catalog...")
-        faculty_codes = sorted(get_faculties_from_catalogue()) # ['ED', 'EN', 'SC', ...]
+        faculty_codes = sorted(get_faculties_from_catalogue())  # ['ED', 'EN', 'SC', ...]
         print(f"Read {len(faculty_codes)} faculties from catalog.\n")
         print(f"Reading subjects from faculties...")
         subjects = []
         for faculty_code in faculty_codes:
             subjects += get_subjects_from_faculty(faculty_code)
-        subjects = sorted(set(subjects)) # ['CHEM, 'CMPUT', 'MATH', ...]
+        subjects = sorted(set(subjects))  # ['CHEM, 'CMPUT', 'MATH', ...]
     raw_data = []
     print(f"Read {len(subjects)} subjects.\n")
-    #time.sleep(3)
+    # time.sleep(3)
     failures = []
     for i, subject in enumerate(subjects):
         print(f"Reading {subject} ({i + 1}/{len(subjects)})...")
         if debug:
             course_nums = ['352']
         else:
-            course_nums = set(get_catalogs_from_subject(subject)) # ['101', '174', ...]
+            course_nums = set(get_catalogs_from_subject(subject))  # ['101', '174', ...]
         course_nums = sorted(course_nums)
         print(f"Reading {len(course_nums)} course{'s' if len(course_nums) != 1 else ''} in {subject}...")
         for course_num in course_nums:
-            #if course_num != "330":
+            # if course_num != "330":
             #   continue
             print(f"Reading {subject} {course_num}")
             try:
@@ -165,10 +178,11 @@ def main():
                     raw_data.append(raw_obj)
             except:
                 failures.append(f"{subject} {course_num}")
-            #time.sleep(2.5)
+            # time.sleep(2.5)
         print(f"Done reading {subject}.\n")
         print(f"Failures: {failures}")
     raw_file.write(json.dumps(raw_data, sort_keys=True, indent=4))
     raw_file.close()
+
 
 main()
